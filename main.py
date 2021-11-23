85a026053d16e4caeb5fc15e127c5c4a3e478659
Apells1 <57453929+Apells1@users.noreply.github.com>
2021-11-23 13:12:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from pathlib import Path
 
+in_file_path = Path.cwd / "webserver-june-2011.txt"
+with in_file_path.open as my_file:
+  for line in my_file.readlines():
+    if "bobsmart.html" in line:
+      location = line.find("domain1335920")+14
+      ip_addr = line[location.location+15].split(" ")[0]
+      print(f"{line[0:20]} {ip_addr}")
+      
